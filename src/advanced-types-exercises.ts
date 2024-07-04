5aa38b128b01a5163016f4fd749d4683ef19e651
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-04 13:43:03 +0200

    Created Pet type which is a union type of Bird and Fish - so Pet can be Bird or Fish

--- src/advanced-types-exercises.ts
+++ src/advanced-types-exercises.ts
@@ -1,10 +1,20 @@
 type User = {
-    name: string;
-    age: number;
-    occupation?: string;
-}
+  name: string;
+  age: number;
+  occupation?: string;
+};
 
 let users: User[] = [
   { name: "John Smith", age: 30, occupation: "Software engineer" },
   { name: "Kate Müller", age: 28 },
 ];
+
+type Bird = {
+  fly: () => void;
+};
+
+type Fish = {
+  swim: () => void;
+};
+
+type Pet = Bird | Fish;
