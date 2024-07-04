5d42e9d332f99e4303663ab213be129508931fa7
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-04 13:37:15 +0200

    Created User type alias

---
+++ src/advanced-types-exercises.ts
@@ -0,0 +1,10 @@
+type User = {
+    name: string;
+    age: number;
+    occupation?: string;
+}
+
+let users: User[] = [
+  { name: "John Smith", age: 30, occupation: "Software engineer" },
+  { name: "Kate Müller", age: 28 },
+];
