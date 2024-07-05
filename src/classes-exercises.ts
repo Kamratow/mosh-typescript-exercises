35afb7dd592986391633eb64f90f0ccb86028d1e
Kamil Ratowski <kamilratowski90@gmail.com>
2024-07-05 10:09:27 +0200

    Created Employee class that is a child of Person class

--- src/classes-exercises.ts
+++ src/classes-exercises.ts
@@ -1,7 +1,9 @@
 class Logger {
   constructor(public logFileName: string) {}
 
-  logMessage(message: string) {}
+  logMessage(message: string) {
+    console.log(message);
+  }
 }
 
 class Person {
@@ -11,3 +13,16 @@ class Person {
     return `${this.firstName} ${this.lastName}`;
   }
 }
+
+class Employee extends Person {
+  constructor(firstName: string, lastName: string, private _salary: number) {
+    super(firstName, lastName);
+  }
+
+  get salary() {
+    return this._salary;
+  }
+}
+
+let newEmployee = new Employee("John", "Test", 700);
+console.log(newEmployee.fullName, newEmployee.salary);
